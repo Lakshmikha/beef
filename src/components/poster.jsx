1e2833f7b060a5ed328027179b8c2522819c3cd2
Lakshmikha <lakshmikha.rejith@hotmail.com>
2024-09-22 01:04:22 +0530

    added review stars

--- src/components/poster.jsx
+++ src/components/poster.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 function Poster() {
   return (
-    <div className="flex flex-col w-48 md:w-1/3 space-y-2 mt-8 ml-2 ">
-        <img src="https://image.tmdb.org/t/p/w1280/wQmQgB6jlXV28XyCrCmtGnsKjHS.jpg" 
-        alt="Beef poster" 
-        className="h-1/2 w-1/2 rounded-lg shadow-2xl"/>
+    <div className= "flex flex-col w-full md:w-1/3 space-y-2 mt-8 p-4 ">
+        
+            <img src="https://image.tmdb.org/t/p/w1280/wQmQgB6jlXV28XyCrCmtGnsKjHS.jpg" 
+            alt="Beef poster" 
+            className="rounded-lg shadow-2xl h-auto w-full mb-2"/>
+            <h2 className="text-4xl font-bold text-center w-full text-red-700">BEEF</h2>
+            <span className="text-red-700 text-3xl text-center">★ ★ ★ ★ ☆</span> 
+        
+       
     </div>
   )
 }
