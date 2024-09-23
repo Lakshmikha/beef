be4f5ef4267de22de60403446dcf2591e83820dc
Lakshmikha <lakshmikha.rejith@hotmail.com>
2024-09-23 23:06:53 +0530

    other miscellaneous backend related changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Banner from './components/banner';
 import Poster from './components/poster';
+import EpiComp from './components/EpiComp';
 
 
 function App() {
+  
   return (
   <div className="bg-black min-h-screen text-white">
     <Banner></Banner>
     <Poster></Poster>
+    <EpiComp></EpiComp>
+
 
 
     
